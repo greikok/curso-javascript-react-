c0b7a0c862e0f8ee8672ada330bd2f08c1357bd5
hector hernandez <hectorhernandez@Hector-2.home>
2020-11-28 23:37:36 +0100

    add module 05

---
+++ modules/05/js/app.js
@@ -0,0 +1,9 @@
+video.addEventListener('click', function () {
+
+  this.classList.add('hidden')
+  import('./player.js').then(({ player }) => {
+    setTimeout(() => {
+      player.play()
+    }, 100)
+  })
+})
